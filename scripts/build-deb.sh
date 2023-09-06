36664950a76773843af97be71e263d2d7070facc
Fabian Baena <fabian.baena@samanagroup.com>
2023-09-06 12:08:10 -0400

    add multiple architecture support; install requirements for building package; cleanup after build

--- scripts/build-deb.sh
+++ scripts/build-deb.sh
@@ -6,17 +6,23 @@ set -xe
 
 TEMPDIR=/usr/src
 
+if [ -f requirements.txt ]; then
+    apt install -y python3-pip
+    python3 -m pip install -r requirements.txt
+fi
+
 python3 setup.py build
 python3 setup.py bdist
 
 tarball_path=$(find dist -type f -name \*.tar.gz)
 tarball=$(basename ${tarball_path})
 
-t=${tarball%.linux-x86_64.tar.gz}
+t=${tarball%.linux-*.tar.gz}
 VERSION=${t#*-}
 PACKAGE_NAME=$(sed -n -e "s/^Package: \+//p" ${TEMPDIR}/debian/control)
+PACKAGE_ARCH=$(sed -n -e "s/^Architecture: \+//p" ${TEMPDIR}/debian/control)
 
-BUILD_DIR=${TEMPDIR}/${PACKAGE_NAME}_${VERSION}-1_amd64
+BUILD_DIR=${TEMPDIR}/${PACKAGE_NAME}_${VERSION}-1_${PACKAGE_ARCH}
 mkdir -p ${BUILD_DIR}/DEBIAN
 cp ${TEMPDIR}/debian/control ${BUILD_DIR}/DEBIAN
 if [ -f ${TEMPDIR}/debian/postinst ]; then
@@ -26,3 +32,6 @@ fi
 tar -C ${BUILD_DIR} -xzvf ${tarball_path}
 
 dpkg --build ${BUILD_DIR}
+
+python3 setup.py clean
+rm -Rf build dist ${PACKAGE_NAME}_${VERSION}-1_${PACKAGE_ARCH} samm.egg-info
